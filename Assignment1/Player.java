4e0a196f9cb19ff88582a8cbc93fbb013651c106
Rich Cherngchaosil <richer_po@hotmail.com>
2016-02-07 12:15:52 -0800

    Fixed alpha/beta bugs

--- Assignment1/Player.java
+++ Assignment1/Player.java
@@ -28,113 +28,142 @@ public class Player {
 	
 	public boolean looks(int depth, ArrayList<Boolean> move){
 		
+		if(move.size() + depth > MAXMOVES){
+			depth = 40 - move.size();
+		}
 		
 		Stack<Pruning> pStack = new Stack<Pruning>();
 		
-		//Can't look pass level 40, dept = 41 is max!
-		if(move.size() + depth > MAXMOVES){
-			System.out.println("Trying to look past level 40th.");
-			return true;
-		}
-		
-		//Traverse down the left-most branches
-		for(int i = 0; i < depth; i++){
+		//Each stack level will represent the node, that why there are depth+1 on the stacks
+		pStack.push(new Pruning(-1000f, 10000f, 10000f));
+		for(int i = 0; i <depth; i++){
 			move.add(true);
-			pStack.push(new Pruning(-10000f,10000f));
-		}	
+			pStack.push(new Pruning(-1000f, 1000f, 10000f));
+		}
 		
-		int level = depth;
-		float beta = 100000f, alpha= -100000f;
-		float maxAlpha = -100000f;
-		float leftAlpha = -100000f, rightAlpha = -100000f;
-		boolean direction  = false;
-		while(pStack.size() > 0){
+		float leftAlpha = -10000f, rightAlpha = -10000f;
+		boolean left = true;
+		while(!pStack.isEmpty()){
+			
+			int level = pStack.size();
+			
 			Pruning p = pStack.pop();
+//			System.out.println("Node Alpha: " +p.alpha);
+//			System.out.println("Node Beta: " + p.beta);
+//			System.out.println("Node Value: " + p.value);
+//			System.out.println();
 			
-//			if(beta == 100000f){
-//				beta = p.beta;
-//			}
-//			if(alpha == -100000f){
-//				alpha = p.alpha;
-//			}
-			
-			if(((level % 2 == 0) && level != 0) || beta > p.beta){
-				//alpha = beta;
-				if(alpha < beta && beta < 1000f)
-					alpha = beta;
-				beta = p.beta;
-			}
-			
-			if((level % 2 != 0 && level != 0)  || alpha < p.alpha){
-				//beta = alpha;
-				if(beta > alpha && alpha > 1000f)
-					beta = alpha;
-				alpha = p.alpha;
-			}
-			if(maxAlpha < p.alpha){
-				maxAlpha = p.alpha;
-			}
-			
-			if(pStack.empty() && direction == false){
-				direction = true;
-				leftAlpha = maxAlpha;
-				maxAlpha = -100000f;
-			} else if (pStack.empty() && direction == true){
-				rightAlpha = maxAlpha;
-			}
-			
-			
-			
-			/***** check if at the "bottom"  *****/
-			if(level == depth){
-				float value = gameTree.value(move);
-				System.out.println(value);
-				if(level % 2 == 0){ //Minimizer's turn
-					if(beta > value){
-						beta = value;
-					}
-				} else if (level % 2 != 0){ //Maximizer's turn
-					if(alpha < value){ 
-						alpha = value;
+			if(pStack.empty()){
+//				System.out.println("Root Alpha: " +p.alpha);
+//				System.out.println("Root Beta: " + p.beta);
+				if(left){
+					left = false;
+					leftAlpha = p.alpha;
+				} else {
+					if(p.alpha == leftAlpha){
+						rightAlpha = 0;
+					} else {
+						rightAlpha = p.alpha;
 					}
 				}
-				
-			} 
-				
-			//if not yet, check left
-			if(move.get(move.size() - 1)){
-				//Push current beta & alpha
-				move.set(move.size() - 1, false);
-				pStack.add(new Pruning(alpha, beta));
-				for(int i = level; i < depth; i ++){
-					pStack.add(new Pruning(alpha, beta));
-					level++;
-					move.add(true);
-				}
-				
-			}  
-			else {
-				level--;
-				move.remove(move.size() - 1);
 			}
+			//beta = p.beta;
+			//alpha = p.alpha;
 			
+			//At the bottom of the tree
+			if(level == depth + 1){
+				p.value = gameTree.value(move);
+				System.out.println(p.value);
+				Pruning node = pStack.peek();
+				if( (level-1) % 2 == 0){ //Minimizer's turn
+					if(p.beta > p.value){
+						//beta = value;
+						node.beta = p.value;
+						//node.value = p.value;
+					}
+					if(node.value == 10000f){
+						node.value = p.value;
+					} else if(node.value > p.value && node.value != 10000f){
+						node.value = p.value;
+					}
+					
+				} else if ((level-1) % 2 != 0){ //Maximizer's turn
+					if(p.alpha < p.value){
+						//alpha = value;
+						node.alpha = p.value;
+						//node.value = p.value;
+					}
+					if(node.value == 10000f){
+						node.value = p.value;
+					} else if(node.value < p.value && node.value != 10000f){
+						node.value = p.value;
+					}
+					
+				}
+
+			} else {
+				
+				
+				//check left if not yet and not at the bottom
+				if(move.size() == 0 || move.get(move.size() - 1) ){
+					if(move.size() == 0){
+						move.add(true);
+					}
+					move.set(move.size() - 1, false);
+					pStack.add(p); //push the current node back to the stack
+					pStack.add(new Pruning(p.alpha, p.beta, 10000f));
+					for(int i = level; i < depth; i++){
+						pStack.add(new Pruning(p.alpha, p.beta, 10000f));
+						move.add(true);
+					}
+				} else { //Moving up
+					if(pStack.empty()) // At the root
+						continue;
+					move.remove(move.size() - 1);
+					Pruning node = pStack.peek();
+					if( (level-1) % 2 == 0){ //Minimizer's turn
+						//Check if it's "empty"
+						if(node.beta != 1000f){
+							if(node.beta > p.value){
+								node.beta = p.value;
+								node.value = p.value;
+							}
+						} else {
+							node.beta = p.value;
+							node.value = node.beta;
+						}
+						
+					} else if ((level-1) % 2 != 0){ //Maximizer's turn
+						if( node.alpha != 1000f){
+							if(node.alpha < p.value){
+								node.alpha = p.value;
+								node.value = p.value;
+							}
+						} else {
+							node.alpha = p.value;
+							node.value = node.alpha;
+						}
+					}
+				}
+			}
 			
 			
 			
 		}
-
-
+		
+		
 		System.out.println("LeftAlpha = " + leftAlpha);
 		System.out.println("RightAlpha = " + rightAlpha);
-		return leftAlpha > rightAlpha? true:false;
+		return leftAlpha > rightAlpha;
 	}
 	
 	class Pruning{
-		public float beta,alpha;
+		public float beta,alpha, value;
 		
-		public Pruning(float a, float b){
+		public Pruning(float a, float b, float v){
 			beta = b;
 			alpha = a;
+			value = v;
 		}
 	}
 }
